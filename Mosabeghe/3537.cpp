ebf49a4b866c45953961e42eba5a3137e4044eb1
Ali Sharifi <31433646+alisharifi2000@users.noreply.github.com>
2018-11-28 22:28:14 +0330

    Create 3537.cpp

---
+++ Mosabeghe/3537.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cin>>n,i;
+    cout<<"W";
+    for(i=1;i<=n;i++)
+    {
+        cout<<"o";
+    }
+    cout<<"w"<<"!";
+    return 0;
+}
