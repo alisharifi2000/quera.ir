e83141efe4abca304ef4724b5c3cf3b717461446
Ali Sharifi <31433646+alisharifi2000@users.noreply.github.com>
2018-11-28 22:33:22 +0330

    Create 3429.cpp

---
+++ Mosabeghe/3429.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>100)
+    {
+        cout<<"Steam";
+    }
+    else if(n<0)
+    {
+        cout<<"Ice";
+    }
+    else
+    {
+        cout<<"Water";
+    }
+}
